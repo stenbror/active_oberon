1fb53b66291b25d7ae571c56bb8a55e215fe9cd4
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-10-22 01:25:29 +0200

    New console header message.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod parser;
 
 fn main() {
-    println!("Active-Oberon, 64bits compiler. Written by Richard Magnor Stenbro");
+    println!("Oberon compiler (Active-Oberon, Oberon-2 and Oberon) 64 bits native compiler. [Version 0.0.1] Written by Richard Magnor Stenbro.");
 }
