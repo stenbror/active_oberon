106538f8d1dbf4d351e0a86c2dd7a56ab209a433
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-10-24 19:22:50 +0200

    First two UnitTests for new scanner passed.

--- src/scanner_active_oberon.rs
+++ src/scanner_active_oberon.rs
@@ -3,16 +3,29 @@ use crate::scanner::Symbol;
 
 
 pub trait ActiveOberonScannerMethods {
+    fn new(text: &'static str) -> Self;
     fn get_next_symbol(&mut self) -> Result<Symbol, String>;
-    fn get_char(&self) -> (char, u32, u32);
+    fn get_char(&mut self) -> (char, u32, u32);
     fn peek_char(&self) -> char;
+
 }
 
 pub struct ActiveOberonScanner {
-
+    buffer: Vec<char>,
+    position: u32,
+    line: u32,
+    col: u32
 }
 
 impl ActiveOberonScannerMethods for ActiveOberonScanner {
+    fn new(text: &'static str) -> Self {
+        ActiveOberonScanner {
+            buffer: text.chars().collect(),
+            position: 0,
+            line: 1,
+            col: 1
+        }
+    }
 
     /// This is the main entry for parser to get next valid symbol (Token) with line and col position.
     fn get_next_symbol(&mut self) -> Result<Symbol, String> {
@@ -193,15 +206,95 @@ impl ActiveOberonScannerMethods for ActiveOberonScanner {
                     _ => Ok(Symbol::Greater(line, col))
                 }
             },
+            '[' => Ok(Symbol::LeftBracket(line, col)),
+            ']' => Ok(Symbol::RightBracket(line, col)),
+            '{' => Ok(Symbol::LeftBrace(line, col)),
+            '|' => Ok(Symbol::Bar(line, col)),
+            '}' => Ok(Symbol::LeftBracket(line, col)),
+            '`' => Ok(Symbol::Transpose(line, col)),
+            '?' => {
+                let ch2 = self.peek_char();
+                match ch2 {
+                    '?' => {
+                        ( _ , _ , _ ) = self.get_char();
+                        Ok(Symbol::QuestionMarks(line, col))
+                    },
+                    _ => Ok(Symbol::QuestionMark(line, col))
+                }
+            },
+            '!' => {
+                let ch2 = self.peek_char();
+                match ch2 {
+                    '!' => {
+                        ( _ , _ , _ ) = self.get_char();
+                        Ok(Symbol::ExclaimMarks(line, col))
+                    },
+                    _ => Ok(Symbol::ExclaimMark(line, col))
+                }
+            },
             _ => Err(format!("Lexical Error => Line: {}, Col: {} - Unknown character '{}' found in source code!", line, col, ch ))
         }
     }
 
-    fn get_char(&self) -> (char, u32, u32) {
-        todo!()
+    fn get_char(&mut self) -> (char, u32, u32) {
+        let mut res = '\0';
+        match self.buffer.get(self.position as usize) {
+            Some(x) => {
+                match x {
+                    '\r' => {
+                        self.position = self.position + 1;
+                        match self.buffer.get(self.position as usize) {
+                            Some('\n') => {
+                                self.position = self.position + 1;
+                                res = '\n'
+                            },
+                            _ => res = '\n'
+                        }
+                        self.line = self.line + 1;
+                        self.col = 1;
+                    },
+                    '\n' => {
+                        self.position = self.position + 1;
+                        self.line = self.line + 1;
+                        self.col = 1;
+                        res = '\n'
+                    },
+                    _ => {
+                        self.position = self.position + 1;
+                        res = x.clone();
+                        self.col = self.col + 1
+                    }
+                }
+
+            },
+            _ => ()
+        }
+        (res, self.line, self.col - 1)
     }
 
     fn peek_char(&self) -> char {
-        todo!()
+        match self.buffer.get(self.position as usize) {
+            Some(x) => {
+                return x.clone()
+            },
+            _ => '\0'
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn scanner_operator_unequal() {
+        let mut scanner = ActiveOberonScanner::new("#");
+        assert_eq!(scanner.get_next_symbol(), Ok(Symbol::NotEqual(1,1)))
+    }
+
+    #[test]
+    fn scanner_operator_and() {
+        let mut scanner = ActiveOberonScanner::new("&");
+        assert_eq!(scanner.get_next_symbol(), Ok(Symbol::And(1,1)))
     }
 }
