c89a0f73de245372790a6e95eb006ef0eccf8488
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-11-05 15:25:28 +0100

    Started on command line parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 mod scanner_active_oberon;
 
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Active-Oberon compiler (2019 Version) 64 bits native compiler. [Version 0.0.1] Written by Richard Magnor Stenbro.");
+
+    let matches = App::new("Active-Oberon compiler (2019 Version) 64 bits native compiler.")
+        .version("Build: '2023-11-05', Version: '0.1'")
+        .author("Richard Magnor Stenbro. stenbror@hotmail.com")
+        .about("A native code compiler for Active Oberon language for use on Linux machines.")
+        .arg(Arg::with_name("file")
+            .short('f')
+            .long("file")
+            .takes_value(true)
+            .help("main source file 'name'[.mod]"))
+        .get_matches();
 }
